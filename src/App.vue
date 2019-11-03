dd4294398fe80540f20fd04e301ab8e533c87ac0
태민 <48385263+UniTaeMin@users.noreply.github.com>
2019-11-04 01:18:41 +0900

    main

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,7 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <button @click="install()">모바일</button>
-    <router-view/>
+    <!-- <button @click="install()">모바일</button> -->
+    <router-view></router-view>
   </div>
 </template>
 <script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,36 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="Main__warp">
+      <img class="Main__Title" src="../assets/sliceLogo.png" width="300px" height="300px"/>
+      <input class="Main__Input" />
+      <div class="Main__Create">코드생성</div>
+      <div>모바일 앱 다운</div>
   </div>
 </template>
-
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'home',
-  components: {
-    HelloWorld
-  }
+  data() {
+    return {
+    };
+  },
 }
 </script>
+<style>
+.Main__warp{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    width: 100%;
+    height:100vh;
+}
+.Main__Input{
+    padding: 15px;
+    border-radius: 20px;
+    box-shadow: 3px 3px 5px;
+    min-width: 300px;
+    margin:10px;
+}
+.Main__Create{
+    margin: 20px;
+}
+</style>
