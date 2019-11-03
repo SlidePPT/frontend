b9dd1cd55377f55da76738056632903757e7feb6
태민 <48385263+UniTaeMin@users.noreply.github.com>
2019-11-03 23:27:55 +0900

    vuetify 제거

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,18 @@
 <template>
-  <v-app>
-    <v-app-bar app>
-      <v-toolbar-title class="headline text-uppercase">
-        <span>Vuetify</span>
-        <span class="font-weight-light">MATERIAL DESIGN</span>
-      </v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-btn
-        @click="install()"
-      >
-        <span class="mr-2">모바일 버전 다운</span>
-      </v-btn>
-    </v-app-bar>
-
-    <v-content>
-      <HelloWorld/>
-    </v-content>
-  </v-app>
+  <div id="app">
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <button @click="install()"></button>
+  </div>
 </template>
-
 <script>
-import HelloWorld from './components/HelloWorld';
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld,
-  },
-  data(){
-    return{
+  data() {
+    return {
       deferredPrompt: null,
-    }
+    };
   },
   mounted() {
     window.addEventListener("beforeinstallprompt", e => {
@@ -44,5 +26,27 @@ export default {
       this.deferredPrompt.prompt();
     },
   }
-};
+}
 </script>
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
--- vue.config.js
+++ vue.config.js
@@ -1,18 +1,16 @@
+  
 module.exports = {
-  "transpileDependencies": [
-    "vuetify"
-  ],
-  outputDir: "docs",
-  pwa: {
-  name: "club.",
-  themeColor: "#36AFFF",
-  workboxPluginMode: "InjectManifest",
-  workboxOptions: {
-    swSrc: "public/service-worker.js"
-  }
-},
-publicPath: process.env.NODE_ENV === "production" ? "/S_vue_pwa/" : "/",
-  devServer:{
-      https: true
-  }
+    outputDir: "docs",
+    pwa: {
+		name: "club.",
+		themeColor: "#36AFFF",
+		workboxPluginMode: "InjectManifest",
+		workboxOptions: {
+			swSrc: "public/service-worker.js"
+		}
+	},
+	publicPath: process.env.NODE_ENV === "production" ? "/frontend/" : "/",
+    devServer:{
+        https: true
+    }
 }
